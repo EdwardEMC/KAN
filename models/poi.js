011283129be073af39234c29124395d8f5ffca98
EMC <edwardcoad.ec@gmail.com>
2020-04-02 15:10:37 +1030

    place of interest (poi) model complete

---
+++ models/poi.js
@@ -0,0 +1,34 @@
+module.exports = function(sequelize, DataTypes) {
+    var PoI = sequelize.define("PoI", {
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lat: {
+            type: DataTypes.DECIMAL(9,6),
+            allowNull: false
+        },
+        lng: {
+            type: DataTypes.DECIMAL(9,6),
+            allowNull: false
+        }
+    },
+    {
+        freezeTableName: true
+    });
+
+    PoI.associate = function(models) {
+        PoI.belongsTo(models.User, {
+          foreignKey: {
+            allowNull: false
+          },
+          onDelete: "cascade"
+        });
+    };
+
+    return PoI;
+};
