714266765588a08801027870d0f545af09a759f3
EMC <edwardcoad.ec@gmail.com>
2020-04-20 15:42:07 +0930

    removed build from ignore as postscript wasn't working

---
+++ client/build/precache-manifest.9a54d4be89f7da6bcc087bba31fd5f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9530ef13e57ab033ee48cd7097c4e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96067fc89412c678e3a3",
+    "url": "/static/css/main.ae1560b5.chunk.css"
+  },
+  {
+    "revision": "b58f43e4fa4171a55adf",
+    "url": "/static/js/2.1630fa2e.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.1630fa2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96067fc89412c678e3a3",
+    "url": "/static/js/main.98392e4b.chunk.js"
+  },
+  {
+    "revision": "f0fbdb305ce3f16eb9cf",
+    "url": "/static/js/runtime-main.83287f80.js"
+  }
+]);
