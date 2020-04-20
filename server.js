36bb72681e982574426358d31a66bcf66f486223
EMC <edwardcoad.ec@gmail.com>
2020-04-20 14:50:42 +0930

    file path corrected

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ io.on('connection', function(socket){
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 db.sequelize.sync().then(function() {
