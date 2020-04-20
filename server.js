d6220e0a8f9bd707ac81b9aa6e7da06779da12e2
EMC <edwardcoad.ec@gmail.com>
2020-04-20 14:36:40 +0930

    another server fix

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 }
 
 // Define API routes here
@@ -70,7 +70,7 @@ io.on('connection', function(socket){
 
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
