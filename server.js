c75de8eef6759a8dcc912f1c916dc51c3de799ae
EMC <edwardcoad.ec@gmail.com>
2020-04-20 15:36:27 +0930

    updated static directory

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname,"./client/build")));
+  app.use(express.static("./client/build"));
 }
 
 const roomList = [];
